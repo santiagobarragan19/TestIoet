f22e92d3eb5e2d5985bd7a6bad597f25618d9e16
sbarragan <sbarraganp19@gmail.com>
2022-05-31 13:33:55 -0500

    created function that converts the txt file into dictionary

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hola mundo")
+def createDictEmployees(file):
+    employeesDict = {}
+
+    file = open(file, "r")
+    lines = file.readlines()
+
+    for line in lines:
+        name, dates = line.split("=")
+        dates = dates.replace("\n", "")
+        dates = dates.split(",")
+
+        datesDict = {}
+        for date in dates:
+            day = date[0:2]
+            datesDict[day] = date[2:]
+        employeesDict[name] = datesDict
+
+    return employeesDict
+
+
+
+employees = createDictEmployees("times1.txt")
