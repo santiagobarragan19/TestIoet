f90eada25784da5cd721851a84da86720f54ac9c
santiago b <sbarraganp19@gmail.com>
2022-06-03 11:24:37 -0500

    added function to compare if the date crossed

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ def createDictEmployees(file):
 
     return employeesDict
 
+
+def count_coincide(day_employeeA, day_employeeB):
+    day_employeeA = day_employeeA.replace(":","")
+    day_employeeB = day_employeeB.replace(":", "")
+    hoursA = day_employeeA.split("-")
+    hoursB = day_employeeB.split("-")
+    if (hoursA[0] <= hoursB[0] and hoursB[0] <= hoursA[1]) or (hoursA[0] <= hoursB[1] and hoursB[1] <= hoursA[1]):
+        return 1
+    elif (hoursB[0] <= hoursA[0] and hoursA[0] <= hoursB[1]) or (hoursB[0] <= hoursA[1] and hoursA[1] <= hoursB[1]):
+        return 1
+    return 0
+
+
 def getTimes(employees):
     result = []
     for employeeA in list(employees):
@@ -24,14 +37,18 @@ def getTimes(employees):
             if employeeA != employeeB:
                 a = employees[employeeA]
                 b = employees[employeeB]
-                intersections = set(a.items()) & set(b.items())
+                intersections = set(a.keys()) & set(b.keys())
                 if intersections.__len__() > 0:
-                    relation = [employeeA + "-" + employeeB , intersections.__len__()]
+                    count = 0
+                    for day in intersections:
+                        count += count_coincide(a[day], b[day])
+                    relation = [employeeA + "-" + employeeB, count]
                     result.append(relation)
         employees.pop(employeeA)
     return result
 
-employees = createDictEmployees("times1.txt")
+
+employees = createDictEmployees("times2.txt")
 times = getTimes(employees)
 for time in times:
-    print("{}:{}".format(time[0],time[1]))
+    print("{}:{}".format(time[0], time[1]))
